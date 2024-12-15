db577b786a9659d424382098f674e49c00093b0b
André Sequeira <imadeamistaek@gmail.com>
2024-12-15 15:37:08 +0000

    Started building backend for the new Mistaek Website - Blog is going first

---
+++ src/components/BeforeDashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+
+const baseClass = 'before-dashboard'
+
+const BeforeDashboard: React.FC = () => {
+	return (
+		<div className={baseClass}>
+			<h1>Mistaek Dashboard</h1>
+		</div>
+	)
+}
+
+export default BeforeDashboard
+		
